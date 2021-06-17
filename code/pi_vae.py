7296bd5927134577f0267653510beea8fe26f3cd
zhd96 <dz2336@columbia.edu>
2021-06-16 21:12:27 -0400

    add comments to pi-vae code

--- code/pi_vae.py
+++ code/pi_vae.py
@@ -16,17 +16,37 @@ from keras.callbacks import LearningRateScheduler
 
 eps = 1e-7;
 
-## define some utility functions
+############# Section I: define utility functions #############
 def slice_func(x, start, size):
+    """Utility function. We use it to take a slice of tensor start from 'start' with length 'size'. Search tf.slice for detailed use of this function.
+    
+    """
     return tf.slice(x, [0,start],[-1,size])
 
 def perm_func(x, ind):
+    """Utility function. Permute x with given indices. Search tf.gather for detailed use of this function.
+    """
     return tf.gather(x, indices=ind, axis=-1);
 
 squeeze_func = Lambda(lambda x: K.squeeze(x, 1));
 
-## preserving volume nflow
+############# Section II: define encoders and decoders #############
+## The following three functions define GIN volume preserving flow
 def first_nflow_layer(z_input, dim_x):
+    """Define the first layer in GIN flow, which maps z to the cancatenation of z and t(z), t is parameterized by NN. 
+    This is equivalent to GIN model with input as z1:dim_z padding dim_x - dim_z zeros.
+    
+    # Arguments
+        z_input: latents z
+        dim_x: dimension of observations x
+        
+    # Important hyperparameters to adjust:
+        gen_nodes: (integer) number of node used in each hidden layer
+        act_func: (list) activation functions used in the hidden layers and output layer
+        
+    # Returns
+        output (tensor): output of the first layer in GIN flow.
+    """
     gen_nodes = dim_x//4;
     dim_z = z_input.shape.as_list()[-1];
     n_nodes = [gen_nodes, gen_nodes, dim_x-dim_z];
@@ -40,8 +60,21 @@ def first_nflow_layer(z_input, dim_x):
     output = layers.concatenate([z_input, output], axis=-1);
     return output
 
-def affine_coupling_layer(x_input, dd=None):
-    DD = x_input.shape.as_list()[-1]; ## DD needs to be an even number
+def affine_coupling_layer(layer_input, dd=None):
+    """Define each affine_coupling_layer, which maps input x to [x_{1:dd}, x_{dd+1:n} * exp(s(x_{1:dd})) + t(x_{1:dd})].
+    
+    # Arguments
+        layer_input: input of affine_coupling_layer.
+        dd: dimension of input which keeps the same after applying this layer. Default is None, which will set dd as half of the layer_input dimension if the input dimension is an even number, or set dd as the closest integer if the input dimension is an odd number.
+        
+    # Important hyperparameters to adjust:
+        n_nodes: (list) number of nodes in the hidden layer and output layer of functions s and t. Both s and t map from dd to dim(layer_input)-dd.
+        act_func: (list) activation functions used in the hidden layers and output layer of functions s and t.
+        
+    # Returns
+        output (tensor): output of an affine_coupling_layer.
+    """
+    DD = layer_input.shape.as_list()[-1];
     if dd is None:
         dd = (DD//2);
     
@@ -51,17 +84,17 @@ def affine_coupling_layer(x_input, dd=None):
     sum_func = Lambda(lambda x: K.sum(-x, axis=-1, keepdims=True));
     
     ## compute output for s and t functions, both from dd to DD-dd
-    x_input1 = Lambda(slice_func, arguments={'start':0,'size':dd})(x_input);
-    x_input2 = Lambda(slice_func, arguments={'start':dd,'size':DD-dd})(x_input);
+    x_input1 = Lambda(slice_func, arguments={'start':0,'size':dd})(layer_input);
+    x_input2 = Lambda(slice_func, arguments={'start':dd,'size':DD-dd})(layer_input);
     st_output = x_input1;
     
-    n_nodes = [DD//4, DD//4, 2*(DD-dd)-1];   #[DD,DD,DD-1] #
+    n_nodes = [DD//4, DD//4, 2*(DD-dd)-1];
     act_func = ['relu', 'relu', 'linear'];
     for ii in range(3):
         st_output = layers.Dense(n_nodes[ii], activation = act_func[ii])(st_output);
     s_output = Lambda(slice_func, arguments={'start':0,'size':DD-dd-1})(st_output);
     t_output = Lambda(slice_func, arguments={'start':DD-dd-1,'size':DD-dd})(st_output);
-    s_output = clamp_func(s_output); ## keep small values of s
+    s_output = clamp_func(s_output); ## make sure output of s is small
     s_output = layers.concatenate([s_output, sum_func(s_output)], axis=-1); ## enforce the last layer has sum 0
     
     ## perform transformation
@@ -70,39 +103,57 @@ def affine_coupling_layer(x_input, dd=None):
     return output
 
 def affine_coupling_block(x_output, dd=None):
+    """Define affine_coupling_block, which contains two affine_coupling_layer.
+    
+    # Returns
+        output (tensor): output of a GIN block (affine_coupling_block).
+    """
     for _ in range(2):
         x_output = affine_coupling_layer(x_output, dd);
     return x_output
 
-
 ## decoder
-
-### decoder using nflow
-def decode_nflow_func(z_input, n_blk, dim_x, mdl, gen_nodes=None):
+## decoder using GIN flow, used in our paper
+def decode_nflow_func(z_input, n_blk, dim_x, mdl, dd=None):
+    """Define mean(p(x|z)) using GIN volume preserving flow.
+    
+    # Arguments
+        z_input: latents z
+        n_blk: number of affine_coupling_block used in normalizing flow
+        dim_x: dimension of observations x
+        mdl: observation model. If 'poisson', add a softplus transformation to the output; if 'gaussian', directly return output.
+        dd: dimension which keeps the same after applying affine_coupling_layer. Check affine_coupling_layer function for more details. Default is None, will set it in affine_coupling_layer function.
+        
+    # Important hyperparameters to adjust:
+        n_blk
+        dd
+        
+    # Returns
+        output (tensor): output of the decoder network, i.e. mean(p(x|z)).
+    """
+    
+    ## generate permutation indices
     permute_ind = [];
-    #n_blk = 5;
     for ii in range(n_blk):
         np.random.seed(ii);
         permute_ind.append(tf.convert_to_tensor(np.random.permutation(dim_x)));
     
-    ## used zero padding before, but now decide to take transformation
-    #dim_z = z_input.shape.as_list()[-1];
-    #concat_func = Lambda(lambda x: layers.concatenate([x,tf.zeros(shape=(tf.shape(x)[0],dim_x-dim_z))], axis=-1));
-    #z_pad_input = concat_func(z_input);
-    #output = affine_coupling_block(z_pad_input);
-    
+    ## Get output through first_nflow_layer 
     output = first_nflow_layer(z_input, dim_x);
+    
+    ## First permute the input before passing it to each GIN block (affine_coupling_block). Repeat this procedure n_blk times.
     for ii in range(n_blk):
         output = Lambda(perm_func, arguments={'ind':permute_ind[ii]})(output);
-        output = affine_coupling_block(output, gen_nodes);
-        
+        output = affine_coupling_block(output, dd);
+    
+    ## Get the final output, if 'poisson' observation, the output is the firing rate as a function of z_input; if 'gaussian' observation, the output is the mean of gaussian as a function of z_input.
     if mdl == 'poisson':
         softplus_func = Lambda(lambda x: softplus(x));
         output = softplus_func(output)
         
     return output
 
-### decoder using monotone increasing nn
+## decoder using monotone increasing nn [initial try, didn't use it eventually]
 def decode_func(z_input, gen_nodes, dim_x, mdl):
     
     n_nodes = [gen_nodes, gen_nodes, dim_x];
@@ -120,7 +171,20 @@ def decode_func(z_input, gen_nodes, dim_x, mdl):
 
 ## encoder
 def encode_func(x_input, gen_nodes, dim_z): 
+    """Define mean or log of variance of q(z|x).
     
+    # Arguments
+        x_input: observations x
+        gen_nodes: number of nodes in the hidden layer of encoder network
+        dim_z: dimension of latents z
+        
+    # Important hyperparameters to adjust:
+        act_func: (list) activation functions used in the hidden layers and output layer
+        n_layers: number of hidden layers + output layer, now fix as 3 which means that we have 2 hidden layers and 1 output layer
+    # Returns
+        output (tensor): output of the encoder network. We can get the mean of q(z|x) and log of variance of q(z|x) separately by calling this function twice.
+    
+    """
     n_nodes = [gen_nodes, gen_nodes, dim_z];
     act_func = ['tanh', 'tanh', 'linear'];
     n_layers = len(n_nodes);
@@ -132,11 +196,59 @@ def encode_func(x_input, gen_nodes, dim_z):
     
     return output
 
+## The following two functions define the prior of p(z|u) for continuous u and discrete u respectively.
+def z_prior_nn(u_input, dim_z):
+    """Compute the prior mean and log of variance of prior p(z|u) for continuous u. 
+    We assume p(z|u) as gaussian distribution with mean and log of variance parameterized by feed-forward neural network as a function of u.
+    
+    # Arguments
+        u_input (tensor): input labels
+        dim_z: dimension of latent z
+    
+    # Important hyperparameters to adjust:
+        n_hidden_nodes_in_prior: number of nodes used in the hidden layer of prior network, now fix as 20 (mean and log of variance share these 2 hidden layers)
+        act_func: (list) activation functions used in the hidden layers and output layer
+        n_layers: number of hidden layers + output layer, now fix as 3 which means that we have 2 hidden layers and 1 output layer
+        
+    # Returns
+        mean and log of variance of prior p(z|u) (tensors)
+    """
+    n_hidden_nodes_in_prior = 20
+    dim_u = u_input.shape.as_list()[-1];
+    n_nodes = [n_hidden_nodes_in_prior, n_hidden_nodes_in_prior, 2*dim_z];
+    act_func = ['tanh', 'tanh', 'linear'];
+    n_layers = len(n_nodes);
+    output = u_input;
+    
+    for ii in range(n_layers):
+        output = layers.Dense(n_nodes[ii], activation=act_func[ii])(output)
+        
+    ## split the last layer as lam_mean and lam_log_var
+    lam_mean = Lambda(slice_func, arguments={'start':0,'size':dim_z})(output);
+    lam_log_var = Lambda(slice_func, arguments={'start':dim_z,'size':dim_z})(output);
+    return lam_mean, lam_log_var
+
+def z_prior_disc(u_input, dim_z, num_u):
+    """Compute the prior mean and log of variance of prior p(z|u) for discrete u. 
+    We assume p(z|u) as gaussian distribution with mean and log of variance treated as different real numbers for different u.
+    
+    # Arguments
+        u_input (tensor): input labels
+        dim_z: dimension of latent z
+        num_u: number of different labels u
+        
+    # Returns
+        mean and log of variance of prior p(z|u) (tensors)
+    """
+    lam_mean = squeeze_func(layers.Embedding(num_u, dim_z, input_length=1)(u_input))
+    lam_log_var = squeeze_func(layers.Embedding(num_u, dim_z, input_length=1)(u_input))
+    return lam_mean, lam_log_var
+
 def sampling(args):
     """Reparameterization trick by sampling from an isotropic unit Gaussian.
 
     # Arguments
-        args (tensor): mean and log of variance of Q(z|X)
+        args (tensor): mean and log of variance of q(z|x)
 
     # Returns
         z (tensor): sampled latent vector
@@ -150,80 +262,76 @@ def sampling(args):
     return z_mean + K.exp(0.5 * z_log_var) * epsilon
 
 def compute_posterior(args):
+    """Compute the full posterior of q(z|x, u). We assume that q(z|x, u) \prop q(z|x)*p(z|u). Both q(z|x) and p(z|u) are gaussian distributed.
+    
+    # Arguments
+        args (tensor): mean and log of variance of q(z|x) and p(z|u)
+        
+    # Returns
+        mean and log of variance of q(z|x, u) (tensor)
+    """
     z_mean, z_log_var, lam_mean, lam_log_var = args;
     # q(z) = q(z|x)p(z|u) = N((mu1*var2+mu2*var1)/(var1+var2), var1*var2/(var1+var2));
     post_mean = (z_mean/(1+K.exp(z_log_var-lam_log_var))) + (lam_mean/(1+K.exp(lam_log_var-z_log_var)));
     post_log_var = z_log_var + lam_log_var - K.log(K.exp(z_log_var) + K.exp(lam_log_var));
     
     return [post_mean, post_log_var]
-    
-def z_prior_nn(u_input, dim_lam):
-    ## dim_lam should be equal to 2*dim_z if gaussian prior
-    dim_u = u_input.shape.as_list()[-1];
-    n_nodes = [20, 20, dim_lam];
-    act_func = ['tanh', 'tanh', 'linear'];
-    n_layers = len(n_nodes);
-    output = u_input;
-    
-    for ii in range(n_layers):
-        output = layers.Dense(n_nodes[ii], activation=act_func[ii])(output)
-        
-    ## split the last layer as lam_mean and lam_log_var
-    dd = dim_lam//2;
-    lam_mean = Lambda(slice_func, arguments={'start':0,'size':dd})(output);
-    lam_log_var = Lambda(slice_func, arguments={'start':dd,'size':dd})(output);
-    return lam_mean, lam_log_var
 
-def z_prior_disc(u_input, dim_z, num_u):
-    lam_mean = squeeze_func(layers.Embedding(num_u, dim_z, input_length=1)(u_input))
-    lam_log_var = squeeze_func(layers.Embedding(num_u, dim_z, input_length=1)(u_input))
-    return lam_mean, lam_log_var
-
-
-## vae model
+############# Section III: define pi-vae model #############
 def vae_mdl(dim_x, dim_z, dim_u, gen_nodes, n_blk=None, mdl='poisson', disc=True, learning_rate=5e-4):
+    """Define pi-vae model.
+    
+    # Arguments
+        dim_x: dimension of observations x.
+        dim_z: dimension of latents z.
+        dim_u: dimension of input labels u.
+        gen_nodes: number of nodes in the hidden layer of encoder (which maps x to z).
+        n_blk: number of flow blocks used in the decoder (which maps z to x).
+        mdl: type of observations. Currently support 'poisson' and 'gaussian'.
+        disc: Boolean. Whether the input labels are discrete (True) or continuous (False).
+        learning_rate: learning_rate used to optimze the loss function of the pi-vae model.
+        
+    # Returns
+        vae: (tensorflow model) the pi-vae model.
+    """
     ### discrete u, or continuous u (one-hot) as input
 
-    ### input layer
+    ### define input layer
     x_input = layers.Input(shape=(dim_x,));
     z_input = layers.Input(shape=(dim_z,));
 
-    ## conditional distribution of z given u # 2*dim_z if z follows gaussian
+    ### define prior distribution p(z|u) [gaussian]
     if disc:
         u_input = layers.Input(shape=(1,));
         lam_mean, lam_log_var = z_prior_disc(u_input, dim_z, dim_u);
     else:
         u_input = layers.Input(shape=(dim_u,));
-        lam_mean, lam_log_var = z_prior_nn(u_input, 2*dim_z);
-    #lam_log_var = clip_func(lam_log_var);
+        lam_mean, lam_log_var = z_prior_nn(u_input, dim_z);
     
-    ### encoder model
+    ### define encoder model
     z_mean = encode_func(x_input, gen_nodes, dim_z);
     z_log_var = encode_func(x_input, gen_nodes, dim_z);
-    #z_log_var = clip_func(z_log_var);
-    
-    # q(z) = q(z|x)p(z|u) = N((mu1*var2+mu2*var1)/(var1+var2), var1*var2/(var1+var2));
+
     post_mean, post_log_var = Lambda(compute_posterior)([z_mean, z_log_var, lam_mean, lam_log_var]);
     z_sample = Lambda(sampling)([post_mean, post_log_var]);
     encoder = Model(inputs = [x_input, u_input], outputs = [post_mean, post_log_var, z_sample,z_mean,z_log_var,lam_mean, lam_log_var], name='encoder')
 
-    ### decoder model
+    ### define decoder model
     if n_blk is not None: # use nflow
         fire_rate = decode_nflow_func(z_input, n_blk, dim_x, mdl);
-    else:
+    else: # this else part has been deprecated 
         fire_rate = decode_func(z_input, gen_nodes, dim_x, mdl);
-    
-    if mdl == 'poisson':
+    if mdl == 'poisson': # clip the value of fire_rate to make it more numerically stable.
         clip_func = Lambda(lambda x: K.clip(x, min_value=1e-7, max_value=1e7));
         fire_rate = clip_func(fire_rate);
     
     decoder = Model(inputs = [z_input], outputs = [fire_rate], name='decoder')
     
-    ### define vae and loss func
-    ### vae model
+    ### run encoder and decoder to define vae
     post_mean, post_log_var, z_sample, z_mean, z_log_var, lam_mean, lam_log_var = encoder([x_input, u_input])
     fire_rate = decoder([z_sample])
     if mdl == 'gaussian':
+        # if gaussian observation, set the observation noise level as different real numbers and optimize it in loss function.
         one_tensor = layers.Input(tensor=(tf.ones((1,1))))
         obs_log_var = layers.Dense(dim_x, activation='linear', use_bias=False, name='obs_noise')(one_tensor);
         #obs_log_var = clip_func(obs_log_var);
@@ -231,7 +339,7 @@ def vae_mdl(dim_x, dim_z, dim_u, gen_nodes, n_blk=None, mdl='poisson', disc=True
     elif mdl == 'poisson':
         vae = Model(inputs = [x_input, u_input], outputs = [post_mean, post_log_var, z_sample,fire_rate, lam_mean, lam_log_var, z_mean, z_log_var], name='vae')
 
-    ### objective function
+    ### define loss function to optimize
     # min -log p(x|z) + E_q log(q(z))-log(p(z|u))
     # cross entropy
     # q (mean1, var1) p (mean2, var2)
@@ -249,7 +357,8 @@ def vae_mdl(dim_x, dim_z, dim_u, gen_nodes, n_blk=None, mdl='poisson', disc=True
     kl_loss *= -0.5
     vae_loss = K.mean(obs_loglik + kl_loss)
     vae.add_loss(vae_loss)
-    #learning_rate = 5e-4;
+    
+    ### define optimizer
     optimizer = Adam(lr = learning_rate);
     vae.compile(optimizer=optimizer)
     
@@ -261,7 +370,8 @@ def custom_data_generator(x_all, u_one_hot):
         for ii in range(len(x_all)):
             yield ([x_all[ii], u_one_hot[ii]], None)
             
-## simulate data
+############# Section IV: simulate data #############
+########### Code below is to simulate the data used in our paper. TODO: separate the code below to another file ################
 def realnvp_layer(x_input):
     DD = x_input.shape.as_list()[-1]; ## DD needs to be an even number
     dd = (DD//2);
